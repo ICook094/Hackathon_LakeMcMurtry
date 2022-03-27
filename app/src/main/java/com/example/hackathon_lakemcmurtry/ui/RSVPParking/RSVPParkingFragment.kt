fc26fa61fbf3e631938ff0cb7099c7f38f621838
aaronlaszlo <78938157+aaronlaszlo@users.noreply.github.com>
2022-03-27 00:59:26 -0500

    Dropdown menu RSVP

--- app/src/main/java/com/example/hackathon_lakemcmurtry/ui/RSVPParking/RSVPParkingFragment.kt
+++ app/src/main/java/com/example/hackathon_lakemcmurtry/ui/RSVPParking/RSVPParkingFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.hackathon_lakemcmurtry.databinding.FragmentRsvpparkingBinding
@@ -22,17 +21,17 @@ class RSVPParkingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val notificationsViewModel =
             ViewModelProvider(this).get(RSVPParkingViewModel::class.java)
 
         _binding = FragmentRsvpparkingBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textMenu
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+//        val textView: TextView = binding.textMenu
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return binding.root
     }
 
     override fun onDestroyView() {
